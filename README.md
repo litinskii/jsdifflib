d11afaded7d6ed56728871176c0161262e339668
sasali <w.siteee@gmail.com>
2016-05-19 13:24:42 -0700

    Add getSideBySideDiffsArray function. Edit Readme.md

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # jsdifflib
 jsdifflib is a Javascript library that provides  a partial reimplementation of Python's difflib module (specifically, the SequenceMatcher class). 
+
+```sh
+npm install https://github.com/w-site/jsdifflib.git
+```
+
+```js
+import { getInlineDiffsArray, getSideBySideDiffsArray } from 'jsdifflib'
+```
+
+## API Reference
+### getInlineDiffsArray(first, second) ⇒ <code>Array</code>
+Returns array of inline diffs.
+
+| Param | Type | Description |
+| --- | --- | --- |
+| first | <code>String</code>  | Base text to compare |
+| second | <code>String</code> | New text to compare  |
+
+### getSideBySideDiffsArray(first, second) ⇒ <code>Array</code>
+Returns arrays of diff side by side, each item of array (ex. array[2]) have 2 items:
+  - Fist - diff from base text
+  - Second - diff from new text
+
+| Param | Type | Description |
+| --- | --- | --- |
+| first | <code>String</code>  | Base text to compare |
+| second | <code>String</code> | New text to compare  |
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var SequenceMatcher = require('./SequenceMatcher');
 
+
+var stripText = function (text) {
+  return text.replace(/\t/g, "\u00a0\u00a0\u00a0\u00a0")
+};
+
 var stripLinebreaks = function (str) {
   return str.replace(/^[\n\r]*|[\n\r]*$/g, "");
 };
@@ -17,13 +22,32 @@ var stringAsLines = function (str) {
   return lines;
 };
 
-var addCellsInline = function (tidx, tidx2, textLines, change) {
+var getCellsInline = function (tidx, tidx2, textLines, change) {
   return {
     'oldLineNumber': tidx == null ? "" : (tidx + 1).toString(),
     'newLineNumber': tidx2 == null ? "" : (tidx2 + 1).toString(),
     'action': change,
-    'text': textLines[tidx != null ? tidx : tidx2].replace(/\t/g, "\u00a0\u00a0\u00a0\u00a0")
+    'text': stripText(textLines[tidx != null ? tidx : tidx2])
   }
+};
+
+var getCells = function (tidx, tend, textLines, change) {
+  if (tidx < tend) {
+    return {
+      'index': (tidx + 1).toString(),
+      'action': change,
+      'text': stripText(textLines[tidx])
+    };
+  } else {
+    return {
+      'index': null,
+      'action': 'empty'
+    };
+  }
+};
+
+var getCellsIndex = function (tidx, tend) {
+  return (tidx < tend) ? tidx + 1 : tidx;
 };
 
 module.exports = {
@@ -58,20 +82,56 @@ module.exports = {
       for (var i = 0; i < rowcnt; i++) {
 
         if (change == "insert") {
-          toprows.push(addCellsInline(null, n++, second, change));
+          toprows.push(getCellsInline(null, n++, second, change));
         } else if (change == "replace") {
-          if (b < be) toprows.push(addCellsInline(b++, null, first, "delete"));
-          if (n < ne) botrows.push(addCellsInline(null, n++, second, "insert"));
+          if (b < be) toprows.push(getCellsInline(b++, null, first, "delete"));
+          if (n < ne) botrows.push(getCellsInline(null, n++, second, "insert"));
         } else if (change == "delete") {
-          toprows.push(addCellsInline(b++, null, first, change));
+          toprows.push(getCellsInline(b++, null, first, change));
         } else {
           // equal
-          toprows.push(addCellsInline(b++, n++, first, change));
+          toprows.push(getCellsInline(b++, n++, first, change));
         }
 
       }
       for (i = 0; i < toprows.length; i++) rows.push(toprows[i]);
       for (i = 0; i < botrows.length; i++) rows.push(botrows[i]);
+    }
+    return rows;
+  },
+
+  getSideBySideDiffsArray: function (first, second) {
+    first = stringAsLines(first);
+    second = stringAsLines(second);
+
+    if (first == null) {
+      throw "Cannot build diff view, first text is not defined.";
+    }
+    if (second == null) {
+      throw "Cannot build diff view; newTextLines is not defined.";
+    }
+
+    var opcodes = (new SequenceMatcher(first, second)).get_opcodes();
+
+    var rows = [];
+
+    for (var idx = 0; idx < opcodes.length; idx++) {
+      var code = opcodes[idx];
+      var change = code[0];
+      var b = code[1];
+      var be = code[2];
+      var n = code[3];
+      var ne = code[4];
+      var rowcnt = Math.max(be - b, ne - n);
+      var toprows = [];
+
+      for (var i = 0; i < rowcnt; i++) {
+        toprows.push([getCells(b, be, first, change), getCells(n, ne, second, change)]);
+        b = getCellsIndex(b, be, first, change);
+        n = getCellsIndex(n, ne, second, change)
+      }
+
+      for (i = 0; i < toprows.length; i++) rows.push(toprows[i]);
     }
     return rows;
   }
